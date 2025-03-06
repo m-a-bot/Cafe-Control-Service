200aee439b5cb80d0d8a304e996b88bb558656e4
Timur <54447582+m-a-bot@users.noreply.github.com>
2025-03-07 00:21:37 +0300

    fix deployment

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("cafe_app.urls")),
     path("admin/", admin.site.urls),
     path("api/", include("cafe_control.urls")),
-    # path("/", include("cafe_app.urls"))
 ]
